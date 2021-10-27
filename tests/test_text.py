bab83f1ab54e9bf332dc77ad482540cbbdfd63d6
kevinfjiang <kfj2112@columbia.edu>
2021-10-27 12:27:34 -0400

    enabled texting test

--- tests/test_text.py
+++ tests/test_text.py
@@ -5,9 +5,7 @@ class TestText(TestAbstract):
     """Testing the text method of FuncNotify
     DisableTexts is set to True by default because I have limited
     money in my Twilio Accounts
-    """  
-    __test__=False # Not testing because Twilio gives me a limited budget
-                   # Which I've almost entirely used up, lol
+    """ 
     
     def test_Method(self, *args, **kwargs):
         time_func(self.wait_test, use_env=True, update_env=True, NotifyMethod="Text", *args, **kwargs)(**kwargs)
