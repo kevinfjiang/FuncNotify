6596e7673a64cd86ef36f77c63638a8f124f4623
kevinfjiang <kfj2112@columbia.edu>
2021-10-28 04:01:09 -0400

    minor change wrapped class instead of function

--- FuncNotify/__main__.py
+++ FuncNotify/__main__.py
@@ -10,47 +10,43 @@ import re
 
 from FuncNotify import *
 
-class ParseKwargs(argparse.Action):
-    """Parses for the format `arg=val` parses for either strings or bools exclusively, no ints
-    """    
-    translation_dict = {"true":  True,
-                         "True":  True,
-                         "false": False,
-                         "False": False,}
-    
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, dict())
-        for value in values:
-            if value is not None:
-                key, value = value.split('=')
-                value = ParseKwargs.translation_dict.get(value, value) # Ensures bools are properly parsed
-                
-                if key in getattr(namespace, self.dest): # If key in list
-                    if isinstance(getattr(namespace, self.dest)[key], list):
-                         getattr(namespace, self.dest)[key].append(value) # Adds to list
-                    else:
-                        getattr(namespace, self.dest)[key] = [getattr(namespace, self.dest)[key], value] # creates list
-                else:
-                    getattr(namespace, self.dest)[key] = value
 
 
 
 def main():
     parsed_remain_arg = []
-    def kwarg_form(arg):
-        """Validate text in form "key=val" internal func to access
-        parsed_remain_arg to ad to 
-        """
-        if not re.match('^[a-zA-Z0-9_]+(=[^\s]+)$', arg): # Ensures valid formatting
-            parsed_remain_arg.append(arg)
-            return
-        return arg
+    class ParseKwargs(argparse.Action):
+        """Parses for the format `arg=val` parses for either strings or bools exclusively, no ints
+        If it paressess a val that doesn't have a `=`, it's ade to the parsed_remain_arg list
+        """    
+        translation_dict = {"true":  True,
+                            "True":  True,
+                            "false": False,
+                            "False": False,}
+        
+        def __call__(self, parser, namespace, values, option_string=None):
+            setattr(namespace, self.dest, dict())
+            for value in values:
+                
+                if re.match('^[a-zA-Z0-9_]+(=[^\s]+)$', value):
+                    key, value = value.split('=')
+                    value = ParseKwargs.translation_dict.get(value, value) # Ensures bools are properly parsed
+                    
+                    if key in getattr(namespace, self.dest): # If key in list
+                        if isinstance(getattr(namespace, self.dest)[key], list):
+                            getattr(namespace, self.dest)[key].append(value) # Adds to list
+                        else:
+                            getattr(namespace, self.dest)[key] = [getattr(namespace, self.dest)[key], value] # creates list
+                    else:
+                        getattr(namespace, self.dest)[key] = value
+                else:
+                    parsed_remain_arg.append(value) 
     
     parser = argparse.ArgumentParser(
         description="FuncNotify - Be notified securely when your function/script completes. " \
                     "Store all your variables in a `.env` file and let us do the work for you " \
                     "To input arguments, use --kwargs followed by `{arg}=value`")
-    parser.add_argument('-k', '--kwargs', nargs='*', type=kwarg_form, action=ParseKwargs)
+    parser.add_argument('-k', '--kwargs', nargs='*', action=ParseKwargs)
     
     args, remaining_args = parser.parse_known_args()
     
