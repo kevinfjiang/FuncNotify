c2b59691ab7b2a17dc5d9fc19a085f9cb1e24195
kevinfjiang <kfj2112@columbia.edu>
2021-11-14 22:32:34 +0000

    Deploying to gh-pages from @ kevinfjiang/FuncNotify@aa5373d2d0a6ff7347ac718b74c5e3a3722cd214 🚀

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""In order to contribute to TestGeneric, one can either write a new file for a
-new Method to ensure coverage (see `test_slack` for an example). Additionally, you
-can write more coverage for `test_generic` for other test_classes to inheirit. Make sure
-any new NotifyMethods notify Kevin.
-"""
-from tests.test_abstract import *
-from FuncNotify import *
